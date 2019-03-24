81c6abd64659da464a4bd762f6f142460af543a9
jiang-austin <austin.jiang@uwaterloo.ca>
2019-03-23 23:18:20 -0700

    jest test fixes

--- src/Components/Layouts/Header/Header.test.js
+++ src/Components/Layouts/Header/Header.test.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<root />, div);
+  ReactDOM.render( <root / > , div);
   ReactDOM.unmountComponentAtNode(div);
 });
